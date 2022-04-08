72574c08e384e7d6f204e70c0bd36a47e4e2f8b0
Ja1-vieira <jpvieirapinheiro@gmail.com>
2022-04-07 21:08:33 -0300

    Alien e marciano

--- sketch.js
+++ sketch.js
@@ -1,17 +1,30 @@
-var aluno13, aluno13_img;
+var alien_1, alien_1_animations;
+var marciano_1, marciano_1_animation;
+var backgroundImg;
 
 function preload(){
-  aluno13_img = loadImage("./assets/images.png");
+  alien_1_animations = loadAnimation("./assets/alien_1-removebg-preview.png",
+   "./assets/alien_2-removebg-preview.png",
+   "./assets/alien_3-removebg-preview.png",
+   "./assets/alien_4-removebg-preview.png");
+  marciano_1_animation = loadAnimation("./assets/Marciano_1-removebg-preview.png",
+  "./assets/marciano_2-removebg-preview.png",
+  "./assets/marciano_3-removebg-preview.png")
+   backgroundImg = loadImage("./assets/background.png")
   
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  aluno13 = createSprite(width/2, height/2);
-  aluno13.addImage(aluno13_img);
+  alien_1 = createSprite(width/2-150, height/2);
+  alien_1.addAnimation("Alien", alien_1_animations);
+  marciano_1 = createSprite(width/2+150, height/2);
+  marciano_1.addAnimation("Marciano", marciano_1_animation);
+  
 }
 
 function draw() {
-  
+  background("grey");
+  image(backgroundImg,0,0,width,height);
   drawSprites();
 }
